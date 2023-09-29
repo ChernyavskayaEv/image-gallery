ca7dee7d47b6d8d96ac0deb7e4f2fd25aed04569
Evgeniya Chernyavskaya <chev640@gmail.com>
2023-09-29 17:55:35 +0300

    feat: add query string and functionality

--- image-galery/scripts/main.js
+++ image-galery/scripts/main.js
@@ -1,5 +1,9 @@
 const body = document.querySelector('.body');
-const mainContent = document.querySelector('.img-galery');
+const form = document.querySelector('.search-form');
+const formQuery = document.getElementById('form-query');
+const resetButton = document.querySelector('.reset');
+const imgContent = document.querySelector('.img-galery');
+const errorRequest = document.querySelector('.error-request_container');
 let imgColumns = document.querySelectorAll('.img-galery__column');
 
 const checkScreenWidth = (screenWidth) => {
@@ -20,12 +24,12 @@ checkScreenWidth(body.offsetWidth);
 window.addEventListener('resize', () => {
   checkScreenWidth(body.offsetWidth);
   imgColumns.forEach((column) => (column.textContent = ''));
-  showData(imgCollection);
+  showData(imgCollection, query);
 });
 
-const query = 'winter';
+let query = 'nature';
 
-const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=40&client_id=yeTW-Vhxrf7GyzsxJmafm_O-RC3r_F4oFzOmKy8Y65I`;
+let url = `https://api.unsplash.com/search/photos?query=${query}&per_page=40&client_id=yeTW-Vhxrf7GyzsxJmafm_O-RC3r_F4oFzOmKy8Y65I`;
 
 let imgCollection;
 
@@ -34,28 +38,65 @@ const getData = async (url) => {
   const data = await res.json();
   imgCollection = data.results;
 
-  showData(imgCollection);
+  showData(imgCollection, query);
 };
 
 getData(url);
 
-const showData = (imgCollection) => {
+const showData = (imgCollection, queryValue) => {
+  // imgContent.style.height = '0px';
   let activeImgColumns = document.querySelectorAll(
     '.img-galery__column.visible'
   );
   let heightColumns;
 
-  imgCollection.forEach((item) => {
-    heightColumns = [...activeImgColumns].map((item) => item.offsetHeight);
-    let indexMinColumn = heightColumns.findIndex(
-      (value) => value === Math.min(...heightColumns)
-    );
+  if (imgCollection.length > 0) {
+    errorRequest.classList.add('hidden');
+    imgCollection.forEach((item) => {
+      heightColumns = [...activeImgColumns].map((item) => item.offsetHeight);
+      let indexMinColumn = heightColumns.findIndex(
+        (value) => value === Math.min(...heightColumns)
+      );
 
-    const img = document.createElement('img');
-    img.classList.add('img-galery-item');
-    img.src = `${item.urls.regular}`;
-    img.alt = `${item.alt_description}`;
-    [...activeImgColumns][indexMinColumn].append(img);
-  });
-  mainContent.style.height = `${Math.min(...heightColumns)}px`;
+      const img = document.createElement('img');
+      img.classList.add('img-galery-item');
+      img.src = `${item.urls.regular}`;
+      img.alt = `${item.alt_description}`;
+      [...activeImgColumns][indexMinColumn].append(img);
+    });
+
+    setTimeout(() => {
+      imgContent.style.height = `${Math.min(
+        ...[...activeImgColumns].map((item) => item.offsetHeight)
+      )}px`;
+    }, 1000);
+  } else {
+    document.querySelector(
+      '.error-request'
+    ).textContent = `There are no results for «${queryValue}».`;
+    errorRequest.classList.remove('hidden');
+  }
 };
+
+form.addEventListener('change', () => {
+  resetButton.classList.remove('opacity');
+});
+
+form.addEventListener('click', (event) => {
+  if (event.target.classList.contains('reset')) {
+    event.target.classList.add('opacity');
+    formQuery.focus();
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  query = formQuery.value;
+  if (query) {
+    url = `https://api.unsplash.com/search/photos?query=${query}&per_page=40&client_id=yeTW-Vhxrf7GyzsxJmafm_O-RC3r_F4oFzOmKy8Y65I`;
+
+    imgColumns.forEach((column) => (column.textContent = ''));
+    getData(url);
+  }
+});
